bc562ef0ffd9d781d75970a8994dae215ed45297
Lars Swemmer <larsswemmer@gmail.com>
2017-08-08 17:58:47 +0200

    V1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,106 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="With years of experience in the music industry, we know what’s important to artists and entertainment brands.">
+    <meta name="author" content="Future Fanbase">
+    <!-- <link rel="icon" href="../../favicon.ico"> -->
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <title>Future Fanbase - We take online marketing to the next level</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="css/cover.css" rel="stylesheet">
+    <link href="css/ff.css" rel="stylesheet">
+  </head>
 
-            .full-height {
-                height: 100vh;
-            }
+  <body>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <div class="site-wrapper">
 
-            .position-ref {
-                position: relative;
-            }
+      <div class="site-wrapper-inner">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        <div class="cover-container">
 
-            .content {
-                text-align: center;
-            }
+          <div class="inner cover">
+            <h1 class="cover-heading"><span>Future Fanbase<span class="yellow">.</span></span></h1>
+            <p class="lead"><span>With years of experience in the music industry<span class="yellow">,</span></span></p>
+            <p class="lead"><span>we know what’s important to artists and entertainment brands<span class="yellow">.</span></span></p>
+          </div>
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
         </div>
-    </body>
+
+      </div>
+
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-2">
+          <h2>We take online marketing to the next level<span class="pink">.</span></h2>
+        </div>
+        <div class="col-md-8 col-md-offset-2">
+          <p>And love to work for: <span class="artist">R3HAB</span>, <span class="artist">Oliver Heldens</span> &amp; <span class="artist">Purple Haze</span>.</p>
+          <br />
+          <p>We innovate every day looking for the best way to master your online marketing strategy.<br /><br />Together we will set your goals and match them with the preferable budget. In result we will create the perfect strategy with the best combination of advertising tools. </p>
+          <br />
+          <a href="mailto:hello@futurefanbase.com">Get in touch!</a>
+        </div>  
+      </div>
+    </div>
+
+    <hr>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          <h2>Founded by two music passionates<span class="pink">.</span></h2>
+          <p>With years of experience in the music industry we decided it was time to combine our powers: online marketing x technical development.</p>
+        </div>
+      </div>
+
+      <div class="row founded">
+        <div class="col-md-3 col-md-offset-2">
+          <img class="img-responsive" src="img/bo-avatar.jpg">
+          <h4><a target="_blank" href="https://www.linkedin.com/in/bonecaverhagen/">Boneca Verhagen</a><span class="pink">.</span></h4>
+          <small>Co-Founder</small>
+        </div>
+        <div class="col-md-3">
+          <img class="img-responsive" src="img/lars-avatar.jpg">
+          <h4><a class="lars" target="_blank" href="https://www.linkedin.com/in/larsswemmer/">Lars Swemmer</a><span class="blue">.</span></h4>
+          <small>Co-Founder</small>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+
+    <div class="container">
+      <div class="row footer">
+        <div class="col-md-2 col-md-offset-2">
+          <img class="img-responsive" src="img/contact.jpg">
+        </div>
+        <div class="col-md-8">
+          <p>Copyright © 2017 Future Fanbase<span class="pink">.</span></p>
+          <p class="contact">
+            Landstraat, 154 | 1401 ET | Bussum, the Netherlands
+            <br />
+            <br />
+            hello@futurefanbase.com
+            <br />
+            Boneca Verhagen: +31 6 1444 2666
+          </p>  
+        </div> 
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
 </html>
